cb2a400d0ff929ff3aba74a1bce65db465cb0cc0
backSpace001 <45762722+backSpace001@users.noreply.github.com>
2019-05-31 13:26:50 +0530

    Add files via upload

---
+++ create_XML_file.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# import libraries of python OpenCV  
+# where its functionality resides 
+import cv2
+import numpy as np
+from PIL import Image
+import os
+import pickle
+
+
+# In[2]:
+
+
+BASE_DIR = os.path.dirname(os.path.abspath('faces_train.ipynd'))
+image_dir = os.path.join(BASE_DIR, "images")
+
+face_cascade = cv2.CascadeClassifier('F:/dd/Library/etc/haarcascades/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ","-").lower()
+            #print(label, path)
+            
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+                
+            id_ = label_ids[label]
+            #print(label_ids)
+            
+            pil_image = Image.open(path).convert("L")
+            #size = (550,550)
+            #final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(pil_image, "uint8")
+            #print(image_array)
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+            for (x,y,w,h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_labels.append(id_)
+                
+
+
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("face-trainner.yml")
+
+
+# In[3]:
+
+
+BASE_DIR = os.path.dirname(os.path.abspath('faces_train.ipynd'))
+image_dir = os.path.join(BASE_DIR, "images")
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            print(path)
+
+
+# In[ ]:
+
+
+
+
