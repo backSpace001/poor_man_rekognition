dc7b7c67b20c9f279595c5123f3230305479998b
backSpace001 <45762722+backSpace001@users.noreply.github.com>
2019-07-05 20:14:45 +0530

    Add files via upload

---
+++ incomplete_filters.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+from pytesseract import image_to_string
+
+# Path of working folder on Disk
+src_path = "C:/Users/Faiz Khan/Desktop/ddd/text recognition"
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread('C:/Users/Faiz Khan/Desktop/ddd/text recognition/2.png')
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite(src_path + "removed_noise.png", img)
+
+    #  Apply threshold to get image with only black and white
+    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite(src_path + "thres.png", img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+
+    # Remove template file
+    #os.remove(temp)
+
+    return result
+
+
+
+print(get_string(src_path + "cont.jpg") )
+
+
+# In[ ]:
+
+
+
+
